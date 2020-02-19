fa089fc5f09846efd045871e39ae1cc3c0724e6e
EncryptedShoesHD <bizjaktomaz72@gmail.com>
2020-02-19 18:56:22 +0100

    Tweaks to the database.php file.

--- includes/database.php
+++ includes/database.php
@@ -24,6 +24,7 @@ class Database {
     return $this->conn;
   }
 
+  // Close connection
   function disconnect() {
     if($this->conn->ping()) {
       if($this->conn->close()) {
@@ -32,8 +33,63 @@ class Database {
     }
   }
 
-  function select() {
+  /*
+    $dataToSelect:
 
+    This array variable is used to specify what table columns have to be selected using a query.
+    In case of ambiguous column names, make sure to specify the name of the table in front of the column name, like this:
+
+    array(
+      "tableName.coloumnName"
+    )
+
+    If you would like to use the AS keyword, you can achieve it by doing that:
+
+    array(
+      "columnName" => "customColumnName"
+    )
+
+
+    $tableName:
+
+    This variable is used to store the table name from which we are going to select the specified columns.
+
+
+    $joins
+
+    This array variable is used to specify table joins. You have to use key => value type of arrays!
+
+    array(
+      "inner" => array("Table1.Column1 = Table2.Column2", "Table3.Column3 = Table4.Column4"),
+      "right" => array(), // Optional, if you don't want or need other joins than inner ones.
+    )
+
+    Key must always match the type of join you would like to create. It is not required to use all types of joins.
+  */
+  function select($dataToSelect, $tableName, $joins) {
+    if(!$this->conn->ping()) {
+      connect();
+    }
+
+    $query = "SELECT ";
+
+    foreach($dataToSelect as $i => $value) {
+      if($i == null) $query = $query . $dataToSelect[$i] . ", ";
+      else $query = $query . $i . " as $dataToSelect[$i], ";
+    }
+
+    $query = substr($query, 0, -2) . " FROM $tableName ";
+
+    if($joins != null) {
+      foreach($joins as $i => $value) {
+        switch(strtolower($i)) {
+          case "inner":
+            $query = $query . " INNER JOIN "
+        }
+      }
+    }
+
+    echo $query;
   }
 }
 
--- index.php
+++ index.php
@@ -4,7 +4,8 @@ include('includes/database.php');
 
 $database = new Database("redovalnica");
 $database->connect();
-$database->close();
+$database->select(array("person_id" => "pid", 'name'), 'People', array("inner" => array("People.person_id = Employed.person_id")));
+echo '<br>';
 $database->disconnect();
 
 ?>
