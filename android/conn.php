0d0e683bc408946d2d7f42f97f0395b97727648c
Aleksander Grobelnik <aleksander.grobelnik1@gmail.com>
2020-04-01 16:00:25 +0200

    added connection files for android app

---
+++ android/conn.php
@@ -0,0 +1,7 @@
+<?php
+$servername = "db";
+$username = "root";
+$password = "geslozabazo";
+$dbname = "redovalnica_test";
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+?>
---
+++ android/register.php
@@ -0,0 +1,15 @@
+<?php
+require "conn.php";
+$user_name = $_POST["username"];
+$user_pass = $_POST["password"];
+$user_pass = hash('sha256',$user_pass);
+$email = $_POST["email"];
+$query = "select * from Credential where username like '$user_name' and password like '$user_pass'";
+$result = mysqli_query($conn , $query);
+echo mysqli_num_rows($result);
+if(mysqli_num_rows($result)>0){
+    $row = mysqli_fetch_assoc($result);
+    $name = $row['username'];
+    echo "login successful. Welcome $name";
+}else echo " login failed";
+?>
