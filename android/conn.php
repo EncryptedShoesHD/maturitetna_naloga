3537fc1a28ce4a3c2728857d1894590fd722c9e5
Aleksander Grobelnik <aleksander.grobelnik1@gmail.com>
2020-04-02 12:40:05 +0200

    connection fix 03

--- android/conn.php
+++ android/conn.php
@@ -1,12 +1,12 @@
 <?php
-//$servername = "db";
-//$username = "root";
-//$password = "geslozabazo";
-//$dbname = "redovalnica_test";
-
-$servername = "localhost";
+$servername = "db";
 $username = "root";
-$password = "";
-$dbname = "redovalnica";
+$password = "geslozabazo";
+$dbname = "redovalnica_test";
+
+//$servername = "localhost";
+//$username = "root";
+//$password = "";
+//$dbname = "redovalnica";
 $conn = mysqli_connect($servername, $username, $password, $dbname);
 ?> 
